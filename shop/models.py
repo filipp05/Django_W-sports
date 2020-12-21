f5a86db7e7d6c92a333f2b662f006c1b88bfa680
Philip NIkulenko <59737526+filipp05@users.noreply.github.com>
2020-12-21 16:48:41 +0300

    Изменен комментарий для модели продукта

--- shop/models.py
+++ shop/models.py
@@ -1,11 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.db.models import F, ExpressionWrapper, FloatField, Sum
+
+
 # Полиморфизм гарантирует, что объект может вести себя по-разному в разных условиях
 
 
+# git init  git status  git add (. / Названия файлов и папок)  git commit -m "сообщение"
+# git checkout (первые 5 цифр коммита / название ветки)
+
+
 class Product(models.Model):
-    """Товар"""
+    """Модель товара"""
     name = models.CharField(verbose_name="Название", max_length=150, db_index=True)
     description = models.TextField(verbose_name="Описание", null=True, blank=True)
     price = models.FloatField(verbose_name="Цена", help_text="Цену указывать в рублях", db_index=True)
