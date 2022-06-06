b7cb42936d9e0482f704a7946097d6641c1d4b15
Philip NIkulenko <filipp05@inbox.ru>
2022-06-06 16:26:45 +0300

    добавлен файл conf

---
+++ core/conf.py
@@ -0,0 +1,23 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+ALLOWED_HOSTS = ["w-sports.ru"]
+
+DATABASES = {
+    'old': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'w_sports_db',
+        'USER': 'philip',
+        'PASSWORD': 'nikulenko7997'
+    }
+}
+
+SECRET_KEY = '@j9sgq$_elrgl9np-u=n*xvgki6o-wyf71t_r12d56f8r#xmc7'
+
+DEBUG = True
--- core/settings.py
+++ core/settings.py
@@ -1,14 +1,6 @@
 import os
-
+from .conf import BASE_DIR, DEBUG, ALLOWED_HOSTS, SECRET_KEY, DATABASES
 # TODO: засунуть все пароли и тд в отдельный файл
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-SECRET_KEY = '@j9sgq$_elrgl9np-u=n*xvgki6o-wyf71t_r12d56f8r#xmc7'
-
-DEBUG = True
-
-ALLOWED_HOSTS = ["w-sports.ru"]
 
 
 # Application definition
@@ -60,18 +52,6 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'old': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    },
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'w_sports_db',
-        'USER': 'philip',
-        'PASSWORD': 'nikulenko7997'
-    }
-}
 
 AUTH_PASSWORD_VALIDATORS = [
     {
