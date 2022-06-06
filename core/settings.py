e7325c7016851ea6cf6dab3554b973ad9a22ac02
Philip NIkulenko <filipp05@inbox.ru>
2022-06-06 16:57:04 +0300

    исправили настройку статики в коре

--- core/settings.py
+++ core/settings.py
@@ -84,8 +84,8 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [STATIC_DIR]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [STATIC_ROOT]
 
 AUTH_USER_MODEL = 'shop.Customer'
 
