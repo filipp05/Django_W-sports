ece7c5cf60deaa17a6e244535ad1ef1c4a6d9286
j92673989 <filipp05@inbox.ru>
2022-06-13 16:29:56 +0300

    добавили статику в гитигнор

--- core/urls.py
+++ core/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 from shop import views
 
@@ -25,4 +27,4 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('', include('shop.urls')),
 
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
